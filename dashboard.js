7bec938a4ae14c4878a6de049954ff7ff1a71f24
techant <techanthere@gmail.com>
2022-01-21 23:10:40 +0500

    color change over interaction Hover state color change & allowed the right and left arrow keys navigation

--- dashboard.js
+++ dashboard.js
@@ -13,14 +13,14 @@ window.addEventListener("DOMContentLoaded", () => {
   tabList.addEventListener("keydown", e => {
     // Move right
     if (e.keyCode === 39 || e.keyCode === 40 || e.keyCode === 38 || e.keyCode === 37) {
-      if (e.keyCode === 39 || e.keyCode === 40) {
+      if (e.keyCode === 39) {
         tabFocus++;
         // If we're at the end, go to the start
         if (tabFocus >= tabs.length) {
           tabFocus = 0;
         }
         // Move left
-      } else if (e.keyCode === 37 || e.keyCode === 38) {
+      } else if (e.keyCode === 37) {
         tabFocus--;
         // If we're at the start, move to the end
         if (tabFocus < 0) {
@@ -37,11 +37,9 @@ function changeTabs(e) {
   const target = e.target;
   const parentAll = document.querySelector(".main");
   parentAll.querySelectorAll('[aria-selected="true"]').forEach(t => t.setAttribute("aria-selected", false));
-  // parentAll.querySelectorAll('[aria-selected="false"]').forEach(t => t.removeAttribute("disabled"));
   target.setAttribute("aria-selected", true);
-  // target.setAttribute("disabled", true);
   parentAll.querySelectorAll('[role="tabpanel"]').forEach(div => {
   div.setAttribute("hidden", true);
   });
-  let currentTarget = parentAll.querySelector(`#${target.getAttribute("aria-controls")}`).removeAttribute("hidden");
+  parentAll.querySelector(`#${target.getAttribute("aria-controls")}`).removeAttribute("hidden");
 }
