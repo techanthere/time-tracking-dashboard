d6a3b9235f81b72d2f62387b57fbf8b966752eda
techant <techanthere@gmail.com>
2022-01-21 21:03:30 +0500

    navigation through arrow keys

--- dashboard.js
+++ dashboard.js
@@ -12,16 +12,15 @@ window.addEventListener("DOMContentLoaded", () => {
 
   tabList.addEventListener("keydown", e => {
     // Move right
-    if (e.keyCode === 39 || e.keyCode === 37) {
-      // tabs[tabFocus].setAttribute("tabindex", -1);
-      if (e.keyCode === 39) {
+    if (e.keyCode === 39 || e.keyCode === 40 || e.keyCode === 38 || e.keyCode === 37) {
+      if (e.keyCode === 39 || e.keyCode === 40) {
         tabFocus++;
         // If we're at the end, go to the start
         if (tabFocus >= tabs.length) {
           tabFocus = 0;
         }
         // Move left
-      } else if (e.keyCode === 37) {
+      } else if (e.keyCode === 37 || e.keyCode === 38) {
         tabFocus--;
         // If we're at the start, move to the end
         if (tabFocus < 0) {
@@ -29,7 +28,6 @@ window.addEventListener("DOMContentLoaded", () => {
         }
       }
 
-      // tabs[tabFocus].setAttribute("tabindex", 0);
       tabs[tabFocus].focus();
     }
   });
@@ -39,12 +37,11 @@ function changeTabs(e) {
   const target = e.target;
   const parentAll = document.querySelector(".main");
   parentAll.querySelectorAll('[aria-selected="true"]').forEach(t => t.setAttribute("aria-selected", false));
-  parentAll.querySelectorAll('[aria-selected="false"]').forEach(t => t.removeAttribute("disabled"));
+  // parentAll.querySelectorAll('[aria-selected="false"]').forEach(t => t.removeAttribute("disabled"));
   target.setAttribute("aria-selected", true);
-  target.setAttribute("disabled", true);
+  // target.setAttribute("disabled", true);
   parentAll.querySelectorAll('[role="tabpanel"]').forEach(div => {
   div.setAttribute("hidden", true);
   });
-
   let currentTarget = parentAll.querySelector(`#${target.getAttribute("aria-controls")}`).removeAttribute("hidden");
 }
